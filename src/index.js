9791aa44d208cc8b861544af18e45d4dc05a0609
shyun <ari4el@naver.com>
2022-03-08 00:07:01 +0900

    워드클라우드 정리

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import WordCloud from "./WordCloud";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <WordCloud/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
